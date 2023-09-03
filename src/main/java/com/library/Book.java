f670c925ee121c5c3e3e2828cbb8d6209e81d127
Riya <riyasach189@gmail.com>
2023-09-03 17:58:40 +0530

    bacha lo pls

---
+++ src/main/java/com/library/Book.java
@@ -0,0 +1,62 @@
+package library;
+import java.time.LocalTime;
+
+public class Book {
+    private final int bookID;
+    private final String title;
+    private final String author;
+    private boolean status;
+    private LocalTime issue_date;
+
+    //constructor
+    protected Book(int bookID, String title, String author) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.status = false;
+    }
+
+    //getters
+    protected int bookID() {
+        return bookID;
+    }
+    protected String getTitle() {
+        return title;
+    }
+    protected boolean getStatus(){
+        return !status;
+    }
+
+    //setters
+    protected void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    protected void setIssueDate(LocalTime issueDate)
+    {
+        this.issue_date = issueDate;
+    }
+
+    //toString method
+    public String toString()
+    {
+       return ("Book ID - " + this.bookID + "\nTitle - " + this.title + "\nAuthor - " + this.author);
+    }
+
+    //methods
+    protected int calculateFine()
+    {
+        int fine = (LocalTime.now().toSecondOfDay() - issue_date.toSecondOfDay());
+
+        if (fine > 10)
+        {
+            return (fine - 10)*3;
+        }
+
+        else
+        {
+            return 0;
+        }
+    }
+}
+
